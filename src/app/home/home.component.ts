4eea46c233ea7c3f2a2dca7c6e7245d5980e5e20
IT20273408 <it20273408@my.sliit.lk>
2021-12-02 20:52:15 +0530

    adding new changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Name} from './home';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myname: Name={
+    name: 'Jayathmi!'
+  }
 
   constructor() { }
 
